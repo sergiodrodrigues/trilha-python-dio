86ee772b18d408b53fd83ced2c3751c41314277e
Sérgio Rodrigues <sergiodaniel.luisrodrigues@gmail.com>
2025-03-21 13:38:09 +0000

    Update database.py

--- 13 - APIs Assíncronas com FastAPI/desafio/src/database.py
+++ 13 - APIs Assíncronas com FastAPI/desafio/src/database.py
@@ -3,10 +3,17 @@ import sqlalchemy as sa
 
 from src.config import settings
 
+# Inicializa o objeto de banco de dados usando a URL de configuração.
 database = databases.Database(settings.database_url)
+
+# Inicializa os metadados do SQLAlchemy.
 metadata = sa.MetaData()
 
+# Cria a engine do SQLAlchemy com base no ambiente configurado.
 if settings.environment == "production":
+    # Em produção, a conexão é direta.
     engine = sa.create_engine(settings.database_url)
 else:
+    # Em desenvolvimento ou testes, desativa a verificação de threads.
     engine = sa.create_engine(settings.database_url, connect_args={"check_same_thread": False})
+

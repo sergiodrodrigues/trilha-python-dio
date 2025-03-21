ce5fdd9b1e79af750608abdc4d6b7ddf54992e7f
Sérgio Rodrigues <sergiodaniel.luisrodrigues@gmail.com>
2025-03-21 13:40:46 +0000

    Update exceptions.py

--- 13 - APIs Assíncronas com FastAPI/desafio/src/exceptions.py
+++ 13 - APIs Assíncronas com FastAPI/desafio/src/exceptions.py
@@ -1,6 +1,7 @@
+# Definição de uma exceção personalizada para indicar que uma conta não foi encontrada.
 class AccountNotFoundError(Exception):
     pass
 
-
+# Definição de uma exceção personalizada para erros de negócio.
 class BusinessError(Exception):
     pass
